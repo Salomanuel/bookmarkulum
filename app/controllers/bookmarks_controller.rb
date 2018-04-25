9a7bb0e20ad67a5d809e8528da86ebafcc0f9ba6
Salomanuel <salomanuel.aux+code@gmail.com>
2018-04-25 17:47:54 +0200

    start implementing create bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,27 @@
 class BookmarksController < ApplicationController
   def index
     @bookmarks = Bookmark.all
+    @bookmark  = Bookmark.new
   end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save 
+      redirect_to @bookmark
+    else
+      render 'index'
+    end
+  end
+
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+
+  private
+
+    def bookmark_params(params)
+      params.require(:bookmark).permit(
+        :title, :url, :shortening)
+    end
 end
