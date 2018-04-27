ce93149aa962b3334594b146eeb56c09d93b0c4f
Salomanuel <salomanuel.aux+code@gmail.com>
2018-04-27 18:27:18 +0200

    add search engine in the StaticPages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,15 @@ class StaticPagesController < ApplicationController
   def home
     @bookmark = Bookmark.new
   end
+
+  def search
+    if params[:bookmark_title]
+      @bookmark = find_bookmark(params)
+    end
+  end
+
+  private
+    def find_bookmark(params)
+      Bookmark.find_by(title: params[:bookmark_title])
+    end
 end
