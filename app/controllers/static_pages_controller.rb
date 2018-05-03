172df41fc78ee0d1ee6d9befd02c7c4eee2f9097
Salomanuel <salomanuel.aux+code@gmail.com>
2018-05-03 14:08:04 +0200

    messing up with the search, currently broken

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,13 +4,22 @@ class StaticPagesController < ApplicationController
   end
 
   def search
-    if params[:bookmark_title]
-      @bookmark = find_bookmark(params)
-    end
+    # if params[:bookmark_title]
+      # @bookmark = find_bookmark(params)
+      @bookmarks = Bookmark.paginate(page: params[:page])
+      # find_bookmarks(params).paginate(page: params[:page])
+    # end
   end
 
   private
+    # quite simple and stupid search,returns just one exact result
     def find_bookmark(params)
       Bookmark.find_by(title: params[:bookmark_title])
+    end
+
+    # more fuzzy search, returns many results
+    def find_bookmarks(params)
+      title = params[:bookmark_title]
+      Bookmark.where("title LIKE '%#{title}%'")
     end
 end
